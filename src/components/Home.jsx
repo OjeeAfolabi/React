48a5b7a24506658c061b9c74375e68ab6ede8592
OjeeAfolabi <afolabiojee@gmail.com>
2025-11-19 22:47:51 +0100

    react basics

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,25 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:3000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource");
+          }
           return res.json();
         })
         .then((data) => {
           setBlogs(data);
+          setError(null)
           setLoading(false);
         })
         .catch((err) => {
-          console.log(err.msessage);
+          setError(err.message);
+          setLoading(false);
         });
     }, 2000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {loading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
